c266cf28e80ada039d975914279b85d09bca283d
nerjib <kabirnajib0@gmail.com>
2020-05-13 11:23:53 +0100

    packages type get them

--- src/controllers/estimator.js
+++ src/controllers/estimator.js
@@ -76,5 +76,21 @@ router.get('/beneficiaries',Request.logRequest, async (req, res) => {
 
 });
 
+router.get('/beneficiaries/verified',Request.logRequest, async (req, res) => {
+
+  const getAllQ = 'SELECT * FROM beneficiaries where package_type !=$1 order by id desc';
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ, ['']);
+    return res.status(201).send(rows);
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return res.status(400).send({ message: 'User with that EMAIL already exist' });
+    }
+    return res.status(400).send(`${error} jsh`);
+  }
+  //  const output1 = (Estimator.covid19ImpactEstimator(req.body))
+
+});
 
 module.exports =  router;
