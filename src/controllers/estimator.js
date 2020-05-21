2dd51c49e9ba16182e23981ccbfad393e171bd45
nerjib <kabirnajib0@gmail.com>
2020-05-21 21:17:50 +0100

    sort

--- src/controllers/estimator.js
+++ src/controllers/estimator.js
@@ -61,7 +61,7 @@ return res.status(400).send(error);
 });
 router.get('/beneficiaries',Request.logRequest, async (req, res) => {
 
-  const getAllQ = 'SELECT * FROM beneficiaries order by id desc';
+  const getAllQ = 'SELECT * FROM beneficiaries order by coupon desc';
   try {
     // const { rows } = qr.query(getAllQ);
     const { rows } = await db.query(getAllQ);
