a83281aa4bd6a9e12a6451bf14680178d1f78c0f
nerjib <kabirnajib0@gmail.com>
2021-07-14 12:38:01 +0100

    get result by ward

--- src/controllers/vote.js
+++ src/controllers/vote.js
@@ -46,6 +46,18 @@ const db = require('../db/index');
     }
   });  
 
-
+  router.get('/byward/:ward', async (req, res) => {
+    const getAllQ = `SELECT * FROM results where ward= $1`;
+    try {
+      // const { rows } = qr.query(getAllQ);
+      const { rows } = await db.query(getAllQ, [req.params.ward]);
+      return res.status(201).send(rows);
+    } catch (error) {
+      if (error.routine === '_bt_check_unique') {
+        return res.status(400).send({ message: 'User with that EMAIL already exist' });
+      }
+      return res.status(400).send(`${error} jsh`);
+    }
+  });  
 
   module.exports = router;
