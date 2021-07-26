b5849171b8b07e5da76e240d04da728e107c8252
nerjib <kabirnajib0@gmail.com>
2021-07-26 21:45:00 +0100

    add card reader

--- src/controllers/incidents.js
+++ src/controllers/incidents.js
@@ -70,9 +70,9 @@ router.post('/', upload.single('file'),  async(req, res) => {
     try {
     const { rows } = await db.query(createUser, values);
     // console.log(rows);
-   /* if(req.body.incidenttype=='Card reader'){
+    if(req.body.incidenttype=='Card reader'){
     await updateCardReader(req.cardreader,req.body.ward, req.body.puid)
-  }*/
+  }
     return res.status(201).send(rows);
     } catch (error) {
     return res.status(400).send(error);
