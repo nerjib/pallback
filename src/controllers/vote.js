d6c7324695f093685c3475f6ffd50e64b91fe263
nerjib <kabirnajib0@gmail.com>
2021-08-01 21:57:01 +0100

    get aggregate collation center results

--- src/controllers/vote.js
+++ src/controllers/vote.js
@@ -225,6 +225,19 @@ router.post('/', upload.single('file'),  async(req, res) => {
       }
       return res.status(400).send(`${error} jsh`);
     }
+  });
+  router.get('/aggcunits', async (req, res) => {
+    const getAllQ = `SELECT sum(apc) as apc, sum(pdp) as pdp, sum(others) as others, sum(accredited) as accredited FROM cunits`;
+    try {
+      // const { rows } = qr.query(getAllQ);
+      const { rows } = await db.query(getAllQ);
+      return res.status(201).send(rows);
+    } catch (error) {
+      if (error.routine === '_bt_check_unique') {
+        return res.status(400).send({ message: 'User with that EMAIL already exist' });
+      }
+      return res.status(400).send(`${error} jsh`);
+    }
   });  
 
   
